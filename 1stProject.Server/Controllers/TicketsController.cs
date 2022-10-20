ee8853bcfec6dafeff846c6fdcd7b9f1788dfd36
hscjw <hscjw@LAPTOP-JPFCJLPD>
2022-10-20 10:08:13 -0400

    Add project files.

---
+++ 1stProject.Server/Controllers/TicketsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace 1stProject.Server.Controllers
+{
+    public class TicketsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+}
